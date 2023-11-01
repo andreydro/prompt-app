4bfa23e86c50f08a0f47ee956726fb3366cace18
andreydro <andreydro14@gmail.com>
2023-11-01 13:24:40 +0100

    Add user and password for elasticsearch

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,5 +2,11 @@ logger = Logger.new(Rails.root.join('log', "elasticsearch.#{Rails.env}.log"), 1,
 logger.level = Rails.env.development? ? Logger::DEBUG : Logger::WARN
 host = ENV['FOUNDELASTICSEARCH_URL'] || 'http://localhost:9200'
 Elasticsearch::Model.client = Elasticsearch::Client.new(
-  host:, logger:, retry_on_failure: 5, reload_connections: true, sniffer_timeout: 3
+  host:,
+  logger:,
+  user: ENV['ES_USER'],
+  password: ENV['ES_PASSWORD'],
+  retry_on_failure: 5,
+  reload_connections: true,
+  sniffer_timeout: 3
 )
