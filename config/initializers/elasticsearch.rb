0d90fde73912b5f934740d4938121ac395c42dcc
andreydro <andreydro14@gmail.com>
2023-11-01 12:46:35 +0100

    Add elasticsearch initializer

---
+++ config/initializers/elasticsearch.rb
@@ -0,0 +1,6 @@
+logger = Logger.new(Rails.root.join('log', "elasticsearch.#{Rails.env}.log"), 1, 100 * 1024 * 1024)
+logger.level = Rails.env.development? ? Logger::DEBUG : Logger::WARN
+host = ENV['FOUNDELASTICSEARCH_URL'] || 'http://localhost:9200'
+Elasticsearch::Model.client = Elasticsearch::Client.new(
+  host:, logger:, retry_on_failure: 5, reload_connections: true, sniffer_timeout: 3
+)
