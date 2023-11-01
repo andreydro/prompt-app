73ea4609a9f0b96010974c3cd7987e479b1bfd00
andreydro <andreydro14@gmail.com>
2023-11-01 15:27:47 +0100

    Update env variable for es, add user and password

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,9 +1,11 @@
 logger = Logger.new(Rails.root.join('log', "elasticsearch.#{Rails.env}.log"), 1, 100 * 1024 * 1024)
 logger.level = Rails.env.development? ? Logger::DEBUG : Logger::WARN
-host = ENV['BONSAI_URL'] || 'http://localhost:9200'
+host = ENV['FOUNDELASTICSEARCH_URL'] || 'http://localhost:9200'
 Elasticsearch::Model.client = Elasticsearch::Client.new(
   host:,
   logger:,
+  user: ENV['ES_USER'],
+  password: ENV['ES_PASSWORD'],
   retry_on_failure: 5,
   reload_connections: true,
   sniffer_timeout: 3
